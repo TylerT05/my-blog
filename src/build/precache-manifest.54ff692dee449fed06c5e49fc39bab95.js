88bd5e790aebc1e7dd63baaa1fad12a272269f31
TylerT05 <45406334+TylerT05@users.noreply.github.com>
2020-10-15 10:13:26 -0700

    First commit

---
+++ src/build/precache-manifest.54ff692dee449fed06c5e49fc39bab95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227dc5511a0cab52e0bfe038f2d88c66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f63b0183446f6cf21b45",
+    "url": "/static/css/main.6841528b.chunk.css"
+  },
+  {
+    "revision": "f7742bd7ca531cfad47f",
+    "url": "/static/js/2.b09d3ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b09d3ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63b0183446f6cf21b45",
+    "url": "/static/js/main.ce4c3de4.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
